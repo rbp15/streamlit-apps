850a259ef84ec3c07c3da2887a97d9fe6f02afa2
RP <13234172+rbp15@users.noreply.github.com>
2023-08-07 10:45:30 -0400

    Update dynamic-page-load.py

--- dynamic-page-load.py
+++ dynamic-page-load.py
@@ -3,11 +3,11 @@ import streamlit.components.v1 as components
 
 st.set_page_config(page_title="Dynamic Page Loading without a Multi-Page App")
 
-sidebar = st.sidebar()
+
 
 markdown = '''
 ["Link 1"](demo_pages/page.py)
 '''
 
-with sidebar:
-  sidebar.markdown(markdown, unsage_allow_html=True)
+with st.sidebar:
+  st.markdown(markdown, unsage_allow_html=True)
