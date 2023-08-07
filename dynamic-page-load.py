019e0f7bf84a50ea93a2d35fd33226ed38601038
RP <13234172+rbp15@users.noreply.github.com>
2023-08-07 10:59:39 -0400

    Update dynamic-page-load.py

--- dynamic-page-load.py
+++ dynamic-page-load.py
@@ -6,11 +6,11 @@ st.set_page_config(page_title="Dynamic Page Loading without a Multi-Page App")
 
 
 markdown = '''
-  ["Direct Load Link 1"](demo_pages/page.py)
+  ["Direct Load Link 1?"](demo_pages/page.py)
 '''
 
 alternative_idea = '''
-  ["Query String Load"](?load=page.py)
+  ["Query String Load?"](?load=page.py)
 '''
 
 with st.sidebar:
@@ -19,8 +19,8 @@ with st.sidebar:
   st.markdown(alternative_idea, unsafe_allow_html=True)
 
 
-
-st.header("Link target would load here, sidebar maintained, no reload?")
-st.textarea('All links in sidebar currently load a url format such as: "/~/+/demo_pages/page.py" or "/~/+/?load=page.py"')
+st.header("Dynamic Page Loading without a Multi-Page App")
+st.markdown("Link target would load here, sidebar maintained, no reload?  Taget link would be a streamlit page under a folder.  Example:  /demo-pages/page.py")
+st.markdown('All links in sidebar currently load a url format such as: "/~/+/demo_pages/page.py" or "/~/+/?load=page.py"')
 st.divider()
-st.textarea("I don't want a multipage app because I need full control over the links in the left bar.  I have a search field that filters the navigation list - it's working")
+st.markdown("I don't want a multipage app because I need full control over the links in the left bar.  I have a search field that filters the navigation list - it's working")
