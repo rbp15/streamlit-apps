827b7af154b413d6febbb1528ece7acadf8506c1
RP <13234172+rbp15@users.noreply.github.com>
2023-08-07 10:57:52 -0400

    Update dynamic-page-load.py

--- dynamic-page-load.py
+++ dynamic-page-load.py
@@ -21,6 +21,6 @@ with st.sidebar:
 
 
 st.header("Link target would load here, sidebar maintained, no reload?")
-st.text('All links in sidebar currently load a url format such as: "/~/+/demo_pages/page.py" or "/~/+/?load=page.py"')
+st.textarea('All links in sidebar currently load a url format such as: "/~/+/demo_pages/page.py" or "/~/+/?load=page.py"')
 st.divider()
-st.text("I don't want a multipage app because I need full control over the links in the left bar.  I have a search field that filters the navigation list - it's working")
+st.textarea("I don't want a multipage app because I need full control over the links in the left bar.  I have a search field that filters the navigation list - it's working")
